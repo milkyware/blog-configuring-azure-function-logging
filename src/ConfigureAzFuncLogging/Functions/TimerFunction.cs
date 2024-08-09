707956b1c5273e1ef72a736e61f44c4df753b8a0
Cameron Cowen <cameronc94@gmail.com>
2024-08-09 23:33:46 +0100

    added initial az func project sample

---
+++ src/ConfigureAzFuncLogging/Functions/TimerFunction.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+
+namespace ConfigureAzFuncLogging.Functions
+{
+    public class TimerFunction(ILogger<TimerFunction> logger)
+    {
+        [Function(nameof(TimerFunction))]
+        public async Task Run([TimerTrigger("0 0 * * *", RunOnStartup = true)] TimerInfo timerInfo)
+        {
+            logger.LogInformation("Starting timer trigger");
+            await Task.Delay(1000);
+            logger.LogDebug("This is a debug message");
+            await Task.Delay(1000);
+            logger.LogTrace("This is a trace message");
+        }
+    }
+}
