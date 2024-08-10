edede73b90cdfb8633b2c3e00fea5aad64d0e45e
Cameron Cowen <cameronc94@gmail.com>
2024-08-10 23:40:53 +0100

    Added sample service

--- src/ConfigureAzFuncLogging/Functions/TimerFunction.cs
+++ src/ConfigureAzFuncLogging/Functions/TimerFunction.cs
@@ -3,16 +3,12 @@ using Microsoft.Extensions.Logging;
 
 namespace ConfigureAzFuncLogging.Functions
 {
-    public class TimerFunction(ILogger<TimerFunction> logger)
+    public class TimerFunction(ILogger<TimerFunction> logger, SampleService sampleService)
     {
         [Function(nameof(TimerFunction))]
         public async Task Run([TimerTrigger("0 0 * * *", RunOnStartup = true)] TimerInfo timerInfo)
         {
-            logger.LogInformation("Starting timer trigger");
-            await Task.Delay(1000);
-            logger.LogDebug("This is a debug message");
-            await Task.Delay(1000);
-            logger.LogTrace("This is a trace message");
+            await sampleService.RunAsync();
         }
     }
 }
---
+++ src/ConfigureAzFuncLogging/SampleService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace ConfigureAzFuncLogging
+{
+    public class SampleService(ILogger<SampleService> logger)
+    {
+        public async Task RunAsync()
+        {
+            logger.LogInformation("Running sample service");
+            await Task.Delay(1000);
+            logger.LogDebug("This is a debug message");
+            await Task.Delay(1000);
+            logger.LogTrace("This is a trace message");
+        }
+    }
+}
