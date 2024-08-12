5258344d0e0b4f15eb3c41e0480aaf3b422e1127
Cameron Cowen <cameronc94@gmail.com>
2024-08-12 20:16:16 +0100

    reorganised configuring logging examples

--- src/ConfigureAzFuncLogging/Program.cs
+++ src/ConfigureAzFuncLogging/Program.cs
@@ -2,6 +2,7 @@
 using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 var host = new HostBuilder()
     .ConfigureFunctionsWebApplication()
@@ -11,23 +12,26 @@ var host = new HostBuilder()
         services.ConfigureFunctionsApplicationInsights();
         services.AddTransient<SampleService>();
     })
-    //.ConfigureLogging((context, logging) =>
-    //{
-    //    logging.Services
-    //        .Configure<LoggerFilterOptions>(options =>
-    //        {
-    //            // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
-    //            // Log levels can also be configured using appsettings.json. For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
-    //            var rule = options.Rules.FirstOrDefault(rule => rule.ProviderName
-    //            == typeof(Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider).FullName);
+    .ConfigureLogging((context, logging) =>
+    {
+        logging.Services
+            .Configure<LoggerFilterOptions>(options =>
+            {
+                // The Application Insights SDK adds a default logging filter that instructs ILogger to capture only Warning and more severe logs. Application Insights requires an explicit override.
+                // Log levels can also be configured using appsettings.json. For more information, see https://learn.microsoft.com/en-us/azure/azure-monitor/app/worker-service#ilogger-logs
+                var rule = options.Rules.FirstOrDefault(rule => rule.ProviderName
+                == typeof(Microsoft.Extensions.Logging.ApplicationInsights.ApplicationInsightsLoggerProvider).FullName);
 
-    //            if (rule is not null)
-    //                options.Rules.Remove(rule);
-    //        });
+                if (rule is not null)
+                    options.Rules.Remove(rule);
+            });
 
-    //    var section = context.Configuration.GetSection("AzureFunctionsJobHost:Logging");
-    //    logging.AddConfiguration(section);
-    //})
+        // Uncomment to explicitly configure CONFIGUREAZFUNCLOGGING logging level
+        //logging.AddFilter("CONFIGUREAZFUNCLOGGING", LogLevel.Trace);
+
+        var section = context.Configuration.GetSection("Logging");
+        logging.AddConfiguration(section);
+    })
     .Build();
 
 await host.RunAsync();
